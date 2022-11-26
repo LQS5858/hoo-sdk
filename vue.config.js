791b9747320521cef2a6a814c284b6f8701e2d25
Ricardo <LQS5858@163.COM>
2022-11-26 16:29:26 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     css: {
-        extract: false
+        extract: false  //强制将css内连
     },
-
+    productionSourceMap: false,
     configureWebpack: config => {
         config.optimization.minimizer[0].options.terserOptions.compress.drop_console = false
         config.output.libraryExport = 'default'
